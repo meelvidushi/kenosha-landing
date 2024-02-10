24c5d914f3c64984a8523701625d247c1ae38eab
vidushijuno <vidushi@junoux.com>
2024-02-10 02:37:29 -0500

    Website 02/07 update 13

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,7 +14,7 @@ function Team() {
       <div className="p-16 mx-auto max-w-screen-xl lg:p-16">
         <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
-          <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+          <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Kenosha AI was founded by Brad Pruitt, M.D., and Bill Kish.  Together they are leveraging the rapidly accelerating power of GPT-class AI to automate RFP responses and perform other CRO operational tasks that were impossible just months ago.</p>
         </div> 
         <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-1">
         
