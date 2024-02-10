c85a5a5373b867d8369a7a17866b049d76ca71ad
vidushijuno <vidushi@junoux.com>
2024-02-10 01:23:29 -0500

    Website 02/07 update 11

--- src/Display.js
+++ src/Display.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero'; // Import the Hero component
 import Header from './components/Header';
-import Information from './components/Information';
 import Footer from './components/Footer';
-import CTA from './components/CTA';
 import InformationLong from './components/InformationLong';
 import Contact from './components/Contact';
 import Landing from './components/Landing';
